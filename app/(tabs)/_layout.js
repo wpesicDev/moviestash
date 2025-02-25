9891e503cfe02cd18f22a8d57f1ab32b6f736fd6
kujov <kujovic.kenin@gmail.com>
2025-02-25 11:01:34 +0100

    initial structure

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,47 @@
+import { Slot } from "expo-router"
+import { Text } from "react-native"
+import { Tabs } from "expo-router"
+import Ionicons from "@expo/vector-icons/Ionicons"
+import Feather from '@expo/vector-icons/Feather';
+export default function HomeLayout() {
+
+    const tabs = [
+        {
+            name: "home/index",
+            title: "Home",
+            icon: "compass-outline"
+        },
+        {
+            name: "favourites/index",
+            title: "favourites",
+            icon: "heart-outline"
+        },
+        {
+            name: "settings/index",
+            title: "Profile",
+            icon: "person-outline"
+        }
+
+    ]
+    return (
+        <Tabs>
+            {tabs.map((tab) => (
+                <Tabs.Screen
+                    key={tab.name}
+                    name={tab.name}
+                    options={{
+                        title: tab.title,
+                        tabBarIcon: ({ color }) => (
+                            <Ionicons
+                                size={28}
+                                style={{ marginBottom: -3 }}
+                                name={tab.icon}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+            ))}
+        </Tabs>
+    )
+}
---
+++ app/index.js
@@ -0,0 +1,4 @@
+import { Redirect } from "expo-router"
+export default function HomeScreen() {
+    return <Redirect href="/home" />
+}
