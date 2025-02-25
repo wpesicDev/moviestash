390fb6ef0b60a6cb0742b5545853cc3651ea786e
kujov <kujovic.kenin@gmail.com>
2025-02-25 13:47:11 +0100

    add TMDB hook

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,20 +1,68 @@
+import { FlatList } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import useTMDB from '../../../hooks/useTMDB';
+import { useEffect } from 'react';
 
 export default function Index() {
+  const { data, loading, error, getDiscoverContent } = useTMDB();
+
+  useEffect(() => {
+    getDiscoverContent();
+  }, []);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error: {error.message}</Text>;
+
   return (
     <View style={styles.container}>
-      <Text>Home page</Text>
+      <FlatList
+        horizontal={true}
+        data={data?.results}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.movieItem}>
+
+            <Image
+              source={{
+                uri: `https://image.tmdb.org/t/p/w300${item.poster_path}`
+              }}
+              style={{ borderRadius: 12, width: "100%", height: "100%" }}
+              transition={200}
+            />
+
+            <Text style={styles.movieTitle}>{item.poster_path}</Text>
+          </View>
+        )}
+      />
+      
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
+  },
+  item: {
+    marginBottom: 20,
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  image: {
+    width: 150,
+    height: 225,
+    resizeMode: 'cover',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 5,
   },
 });
---
+++ hooks/useTMDB.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+
+const API_KEY = process.env.TMDB_API_KEY;
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const useMovieData = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async (url) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(url, {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+          'accept': 'application/json'
+        }
+      });
+      console.log(response)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getDiscoverContent = (page = 1) => {
+    const url = `${BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`;
+    console.log('fetching discover')
+    fetchData(url);
+  };
+
+  const getMovieDetails = (movieId) => {
+    const url = `${BASE_URL}/movie/${movieId}?language=en-US`;
+    fetchData(url);
+  };
+
+  const getMovieCast = (movieId) => {
+    const url = `${BASE_URL}/movie/${movieId}/credits?language=en-US`;
+    fetchData(url);
+  };
+
+  return {
+    data,
+    loading,
+    error,
+    getDiscoverContent,
+    getMovieDetails,
+    getMovieCast
+  };
+};
+
+export default useMovieData;
