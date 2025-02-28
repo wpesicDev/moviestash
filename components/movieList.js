5f2a324d21ecb384292a2587c831a0b59b8702b7
kujov <kujovic.kenin@gmail.com>
2025-02-28 11:45:36 +0100

    add link to movieList

--- components/movieList.js
+++ components/movieList.js
@@ -2,6 +2,8 @@ import { View, FlatList, Image, StyleSheet } from "react-native";
 import CustomText from "./customText";
 import RatingContainer from "./ratingContainer";
 import { Ionicons } from "@expo/vector-icons";
+import { Link } from 'expo-router';
+
 export default function MovieList({ data }) {
     return (
         <View style={styles.container}>
@@ -12,42 +14,44 @@ export default function MovieList({ data }) {
                 keyExtractor={(item) => item.id.toString()}
                 renderItem={({ item }) => (
                     item.poster_path ? (
-                        <View style={styles.movieItem}>
-                            <Image
-                                source={{
-                                    uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
-                                }}
-                                style={{
-                                    width: 100,
-                                    height: 150,
-                                    borderRadius: 8,
-                                    marginRight: 16,
-                                }}
-                                transition={300}
-                            />
-                            <View style={styles.movieItemTextContainer}>
-                                <CustomText
-                                    variant="title"
-                                    numberOfLines={2}
-                                >{item.title ?? item.name}</CustomText>
-                                <CustomText color='grey'>
-                                    {item.tagline}
-                                </CustomText>
-                                <RatingContainer item={item} style={styles.ratingContainer} />
-                                <View style={styles.lowerContainer}>
-                                    {item.genres && (
-                                        <View style={styles.genreContainer}>
-                                            {item.genres.slice(0, 3).map((genre) => (
-                                                <CustomText key={genre} color="white" style={styles.labels}>
-                                                    {genre}
-                                                </CustomText>
-                                            ))}
-                                        </View>
-                                    )}
-                                    <Ionicons name="heart" size={24} color={"#FF0000"} />
+                        <Link push href={`/${item.title ? 'movie': 'show'}/${item.id}`}>
+                            <View style={styles.movieItem}>
+                                <Image
+                                    source={{
+                                        uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+                                    }}
+                                    style={{
+                                        width: 100,
+                                        height: 150,
+                                        borderRadius: 8,
+                                        marginRight: 16,
+                                    }}
+                                    transition={300}
+                                />
+                                <View style={styles.movieItemTextContainer}>
+                                    <CustomText
+                                        variant="title"
+                                        numberOfLines={2}
+                                    >{item.title ?? item.name}</CustomText>
+                                    <CustomText color='grey'>
+                                        {item.tagline}
+                                    </CustomText>
+                                    <RatingContainer item={item} style={styles.ratingContainer} />
+                                    <View style={styles.lowerContainer}>
+                                        {item.genres && (
+                                            <View style={styles.genreContainer}>
+                                                {item.genres.slice(0, 3).map((genre) => (
+                                                    <CustomText key={genre} color="white" style={styles.labels}>
+                                                        {genre}
+                                                    </CustomText>
+                                                ))}
+                                            </View>
+                                        )}
+                                        <Ionicons name="heart" size={24} color={"#FF0000"} />
+                                    </View>
                                 </View>
                             </View>
-                        </View>
+                        </Link>
                     ) : null
                 )}
             />
