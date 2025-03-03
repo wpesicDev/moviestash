4a3a9e791526b3e7a4a57eca5378fa42e4233fa6
kujov <kujovic.kenin@gmail.com>
2025-03-03 14:27:29 +0100

    fix nested virtualizedList

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,33 +1,18 @@
-import { ScrollView } from "react-native";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  SafeAreaView,
-  TextInput,
-} from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, SafeAreaView, TextInput, FlatList } from "react-native";
 import useTMDB from "../../../hooks/useTMDB";
-import { useEffect, useState } from "react";
 import MovieSlider from "../../../components/movieSlider";
 import CustomText from "../../../components/customText";
 import { useNavigation } from "@react-navigation/native";
 import MovieList from "../../../components/movieList";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Index() {
   const [search, setSearch] = useState("");
   const [debouncedSearch, setDebouncedSearch] = useState(search);
 
-  const {
-    data,
-    loading,
-    error,
-    getDiscoverMovies,
-    getDiscoverShows,
-    getSearchResults,
-  } = useTMDB();
+  const { data, loading, error, getDiscoverMovies, getDiscoverShows, getSearchResults } = useTMDB();
   const navigation = useNavigation();
+
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedSearch(search);
@@ -38,11 +23,8 @@ export default function Index() {
   }, [search]);
 
   useEffect(() => {
-    if (debouncedSearch) {
-      if (search.length >= 3) {
-        getSearchResults(search);
-        console.log(data.search);
-      }
+    if (debouncedSearch && debouncedSearch.length >= 3) {
+      getSearchResults(debouncedSearch);
     }
   }, [debouncedSearch]);
 
@@ -55,13 +37,56 @@ export default function Index() {
       headerBackTitleVisible: false,
     });
   }, []);
+
   if (error) return <Text>Error: {error.message}</Text>;
+
+  const renderContent = () => {
+    if (loading) {
+      return (
+        <View style={styles.notfound}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+
+    if (search !== "" && search.length >= 3) {
+      if (data.search?.results?.length > 0) {
+        return <MovieList data={data.search?.results} />;
+      } else {
+        return (
+          <View style={styles.notfound}>
+            <Text>No results found</Text>
+          </View>
+        );
+      }
+    } else if (search !== "" && search.length < 3) {
+      return (
+        <View style={styles.notfound}>
+          <Text>Search with at least 3 characters</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.homeContainer}>
+          <CustomText variant="title" align="left">
+            Popular Movies
+          </CustomText>
+          <MovieSlider data={data?.movies} />
+          <CustomText variant="title" align="left">
+            Popular Series
+          </CustomText>
+          <MovieSlider data={data?.shows} />
+        </View>
+      );
+    }
+  };
+
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <CustomText variant="display" align="left" style={styles.title}>
         Home
       </CustomText>
-      <ScrollView contentContainerStyle={styles.container}>
+      <View style={{paddingHorizontal: 10}}>
         <TextInput
           style={styles.input}
           onChangeText={setSearch}
@@ -69,58 +94,28 @@ export default function Index() {
           placeholder="Search for Movies, Shows and Actors"
           returnKeyType={"done"}
         />
+      </View>
 
-        {(() => {
-          if (loading) {
-            return (
-              <View style={styles.notfound}>
-                <Text>Loading...</Text>
-              </View>
-            );
-          }
-          if (search !== "" && search.length >= 3) {
-            if (data.search?.results?.length > 0) {
-              return (
-                <View>
-                  <MovieList data={data.search?.results} />
-                </View>
-              );
-            } else {
-              return (
-                <View style={styles.notfound}>
-                  <Text>No results found</Text>
-                </View>
-              );
-            }
-          } else if (search !== "" && search.length < 3) {
-            return (
-              <View style={styles.notfound}>
-                <Text>Search with at least 3 characters</Text>
-              </View>
-            );
-          } else {
-            return (
-              <View>
-                <CustomText variant="title" align="left">
-                  Popular Movies
-                </CustomText>
-                <MovieSlider data={data?.movies} />
-                <CustomText variant="title" align="left">
-                  Popular Series
-                </CustomText>
-                <MovieSlider data={data?.shows} />
-              </View>
-            );
-          }
-        })()}
-      </ScrollView>
+      <FlatList
+        data={[null]}
+        renderItem={() => <>{renderContent()}</>}
+        keyExtractor={(item, index) => index.toString()}
+        contentContainerStyle={styles.container}
+        showsVerticalScrollIndicator={false}
+      />
     </SafeAreaView>
   );
 }
+
 const styles = StyleSheet.create({
+  homeContainer: {
+    display: 'flex',
+    gap: 10
+  },
   notfound: {
     height: 300,
     justifyContent: "center",
+    alignItems: "center",
   },
   input: {
     height: 40,
@@ -133,27 +128,16 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   safeAreaView: {
+    height: 'auto',
     backgroundColor: "#fff",
-    display: "flex",
     flex: 1,
+    maxHeight: 'auto'
   },
   container: {
     flexGrow: 1,
     alignItems: "center",
     justifyContent: "flex-start",
     padding: 10,
-  },
-  item: {
-    marginBottom: 20,
-    alignItems: "center",
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
-  },
-  image: {
-    width: 10,
-    height: 225,
-    resizeMode: "cover",
   },
   title: {
     paddingLeft: 10,
